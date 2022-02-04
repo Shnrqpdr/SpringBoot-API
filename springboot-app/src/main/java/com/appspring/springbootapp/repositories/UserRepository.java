24ab57b3c466f04d794e81ff208602c56ce2f665
Pedro <hnrqpdr.sc@gmail.com>
2022-02-03 22:46:14 -0300

    Service layer, component registration

--- springboot-app/src/main/java/com/appspring/springbootapp/repositories/UserRepository.java
+++ springboot-app/src/main/java/com/appspring/springbootapp/repositories/UserRepository.java
@@ -3,7 +3,9 @@ package com.appspring.springbootapp.repositories;
 import com.appspring.springbootapp.entities.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     
 }
