31cf485ec55a2649dcd0f260896f6786020777f1
Pedro <hnrqpdr.sc@gmail.com>
2022-02-03 22:27:52 -0300

    JPA repository, dependency injection, database seeding

---
+++ springboot-app/src/main/java/com/appspring/springbootapp/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.appspring.springbootapp.repositories;
+
+import com.appspring.springbootapp.entities.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    
+}
