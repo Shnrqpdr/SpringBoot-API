9308687dde4e43b587bc547843e078a1f9be5beb
Pedro <hnrqpdr.sc@gmail.com>
2022-06-24 00:28:56 -0300

    Tratamento de exceções no update

--- springboot-app/src/main/java/com/appspring/springbootapp/services/UserService.java
+++ springboot-app/src/main/java/com/appspring/springbootapp/services/UserService.java
@@ -4,6 +4,8 @@ import com.appspring.springbootapp.repositories.UserRepository;
 import com.appspring.springbootapp.services.exceptions.DatabaseException;
 import com.appspring.springbootapp.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -43,9 +45,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
